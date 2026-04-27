99a3ffb0203f4142704e3402c31f95547b11d254
Aaron <aaronklinker1@gmail.com>
2026-04-27 16:01:29 -0500

    chore: Debug action logging

--- src/actions/release.ts
+++ src/actions/release.ts
@@ -1,8 +1,18 @@
-import { getBooleanInput, getInput, getMultilineInput, setFailed, setOutput } from "@actions/core";
+import {
+  getBooleanInput,
+  getInput,
+  getMultilineInput,
+  info,
+  setFailed,
+  setOutput,
+} from "@actions/core";
 
 import type { NullablyRequired } from "../internal/utils";
 import { release, type ReleaseOptions } from "../release";
 import { getFilesInput } from "./internal/get-files-input";
+
+console.log("Hello world");
+info("Hello info");
 
 try {
   const options: NullablyRequired<ReleaseOptions> = {
