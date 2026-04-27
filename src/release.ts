905b141d1bb9f0d3be24be2e0a29eae84dd7d9c3
Aaron <aaronklinker1@gmail.com>
2026-04-27 17:01:09 -0500

    chore: Debug conventional commit parser

--- src/release.ts
+++ src/release.ts
@@ -448,9 +448,9 @@ export async function release(options: ReleaseOptions): Promise<ReleaseMeta> {
   const since = options.since ?? (await findPreviousTag(tagPrefix));
   console.log("Since:", since);
   const commits = await listCommitsSince({ since, dirs: [path, ...additionalDirs] });
-  console.log("Commits:", commits.length, commits);
+  console.log("Commits:", commits.length);
   const conventionalCommits = parseCommits(commits);
-  console.log("Conventional commits:", conventionalCommits.length, conventionalCommits);
+  console.log("Conventional commits:", conventionalCommits.length, conventionalCommits[0]);
 
   // 3. Bump version
 
