2379bcd521acd6aa35ee3e9e0cbdac50a5c8cb4d
Aaron <aaronklinker1@gmail.com>
2026-05-18 14:21:23 -0700

    fix: Use `--merged=HEAD` when looking up previous git tag so only ones in the current commit history are included (#7)

--- src/internal/run-git-tag.ts
+++ src/internal/run-git-tag.ts
@@ -3,9 +3,13 @@ import { spawn } from "node:child_process";
 import { waitForChildProcess } from "./wait-for-child-process";
 
 export async function runGitTag(prefix: string, cwd: string = process.cwd()): Promise<string> {
-  const child = spawn("git", ["--no-pager", "tag", "-l", `${prefix}*`, "--sort=-version:refname"], {
-    cwd,
-  });
+  const child = spawn(
+    "git",
+    ["--no-pager", "tag", "-l", `${prefix}*`, "--merged=HEAD", "--sort=-version:refname"],
+    {
+      cwd,
+    },
+  );
   const { stdout } = await waitForChildProcess(child);
   return stdout;
 }
